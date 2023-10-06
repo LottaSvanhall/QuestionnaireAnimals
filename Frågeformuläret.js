5fbd52e3095226f40d03f231cd5e18da6fbdde66
LottaSvanhall <charlotte.svanhall@live.se>
2023-10-06 09:09:39 +0200

    Saved two files
    Two files uploaded to the questionnaire project

--- Frågeformuläret.js
+++ Frågeformuläret.js
@@ -1,9 +1,8 @@
 const prompt = require('prompt-sync')({ sigint: true });
 const question = require('./questions.json');
-const result = require('../results.json');
-//console.log(result);
+const result = require('./results.json');
 
-const fs = require('fs'); //fs ger oss möjlighet att skriva till filer
+const fs = require('fs');
 
 let name = (prompt("Skriv in ditt namn: "));
 console.log(`${name}`);
@@ -15,84 +14,74 @@ const userData =
 {
   Namn: name,
   Date: Date(),
-};
 
-const AnimalScore =
+  Result: [
+    {
+      Animal: "Katt",
+      Score: 0
+    },
+    {
+      Animal: "Hund",
+      Score: 0
+    },
+    {
+      Animal: "Fisk",
+      Score: 0
+    },
+    {
+      Animal: "Kanin",
+      Score: 0
+    }
+  ]
+}
 
-{
-  "Katt": 0, //sätter till noll för att nyckeln ska vara en siffra
-  "Hund": 0,
-  "Fisk": 0,
-  "Kanin": 0
-};
 
 for (let i = 0; i < animalQuestions.length; i++) {
-  console.log(`${animalQuestions[i].MyQuestion}` + "\nSvara 1 eller 2: " + "\n 1 = Ja" + "\n 2 = Nej"); //hämta fråga i json"
+  console.log(`${animalQuestions[i].MyQuestion}` + "\nSvara 1 eller 2: " + "\n 1 = Ja" + "\n 2 = Nej");
 
   let svar = Number(prompt().trim())
   if (svar == 1) {
-    AnimalScore.Katt += animalQuestions[i].Ja.Katt;
-    AnimalScore.Hund += animalQuestions[i].Ja.Hund;
-    AnimalScore.Fisk += animalQuestions[i].Ja.Fisk;
-    AnimalScore.Kanin += animalQuestions[i].Ja.Kanin;
+    userData.Result[0].Score += animalQuestions[i].Ja.Katt;
+    userData.Result[1].Score += animalQuestions[i].Ja.Hund;
+    userData.Result[2].Score += animalQuestions[i].Ja.Fisk;
+    userData.Result[3].Score += animalQuestions[i].Ja.Kanin;
   }
 
-  //console.log(animalQuestions[i].Ja.Katt, animalQuestions[i].Ja.Hund, animalQuestions[i].Ja.Fisk, animalQuestions[i].Ja.Kanin);
+  else if (svar == 2) {
+    userData.Result[0].Score += animalQuestions[i].Nej.Katt;
+    userData.Result[1].Score += animalQuestions[i].Nej.Hund;
+    userData.Result[2].Score += animalQuestions[i].Nej.Fisk;
+    userData.Result[3].Score += animalQuestions[i].Nej.Kanin;
 
-  else if (svar == 2)
-  //(svar.toLowerCase().trim() == "nej")
-  {
-    AnimalScore.Katt += animalQuestions[i].Nej.Katt;
-    AnimalScore.Hund += animalQuestions[i].Nej.Hund;
-    AnimalScore.Fisk += animalQuestions[i].Nej.Fisk;
-    AnimalScore.Kanin += animalQuestions[i].Nej.Kanin;
-    //console.log(animalQuestions[i].Nej.Katt, animalQuestions[i].Nej.Hund, animalQuestions[i].Nej.Fisk, animalQuestions[i].Nej.Kanin);
   }
   else {
     (svar = isNaN)
-    i-- //för att stanna kvar på samma fråga, i minskas med 1
+    i--
     console.log("Du måste skriva in 1 för Ja eller 2 för Nej!")
   }
 }
 
-const totalPoints = AnimalScore.Katt + AnimalScore.Hund + AnimalScore.Fisk + AnimalScore.Kanin;
+const totalPoints = userData.Result[0].Score + userData.Result[1].Score + userData.Result[2].Score + userData.Result[3].Score;
 
 
-AnimalScore.Katt = (AnimalScore.Katt / totalPoints * 100);
-AnimalScore.Hund = (AnimalScore.Hund / totalPoints * 100);
-AnimalScore.Fisk = (AnimalScore.Fisk / totalPoints * 100);
-AnimalScore.Kanin = (AnimalScore.Kanin / totalPoints * 100);
+userData.Result[0].Score = (userData.Result[0].Score / totalPoints * 100);
+userData.Result[1].Score = (userData.Result[1].Score / totalPoints * 100);
+userData.Result[2].Score = (userData.Result[2].Score / totalPoints * 100);
+userData.Result[3].Score = (userData.Result[3].Score / totalPoints * 100);
 
-//console.log(AnimalScore.Katt + "%");
-//AnimalScore.Katt += "%"; //för att slippa skriva in userData.Katt två ggr
-AnimalScore.Katt = AnimalScore.Katt + "%"; //konkatenering dvs. omvandla ett tal till en sträng
-AnimalScore.Hund = AnimalScore.Hund + "%";
-AnimalScore.Fisk = AnimalScore.Fisk + "%";
-AnimalScore.Kanin = AnimalScore.Kanin + "%";
-
-/*function compareNumbers(a, b) {
-  return a - b;
-}*/
-
-const SorteraArray = [AnimalScore.Katt, AnimalScore.Hund, AnimalScore.Fisk, AnimalScore.Kanin];
-for (let i = 0; i < SorteraArray.length; i++);
 {
-  SorteraArray.sort(); // [1, 5, 40, 200]
-  SorteraArray.reverse();
+  userData.Result.sort((a, b) => a.Score - b.Score);
+  userData.Result.reverse();
 }
 
-//Sortera.unshift(userData.Namn) //först i listan
-//Sortera.push(userData.Date) //sist i listan
-//console.log(SorteraArray);
-//}
+userData.Result[0].Score = userData.Result[0].Score + "%";
+userData.Result[1].Score = userData.Result[1].Score + "%";
+userData.Result[2].Score = userData.Result[2].Score + "%";
+userData.Result[3].Score = userData.Result[3].Score + "%";
 
-
-const ResultArray = [result, userData, AnimalScore, SorteraArray]; //hämtar det som finns i result.json och userData och sparar i resultatArrayen
-//console.log(ResultArray);
-
+const ResultArray = [result, userData];
 
 fs.writeFile('./results.json', JSON.stringify(ResultArray, null, 2), (err) => {
   if (err) throw err;
-  //bytte ut userData mot ResultArray i stringify ovan för att kunna spara in de olika resultaten.
   console.log('Data written to file')
 });
